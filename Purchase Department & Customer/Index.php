03fead10214346496bcba43b8ec28430e1d5a03a
Tanha Reja <77929567+TanhaR31@users.noreply.github.com>
2021-04-29 01:28:54 +0600

    Index

---
+++ Purchase Department & Customer/Index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Dealer Management System</title>
+  <link rel = "icon" href = "https://icons.iconarchive.com/icons/mag1cwind0w/akisame/128/Leaf-icon.png">
+  <link rel="stylesheet" href="CSS/form_style.css">
+</head>
+
+<body>
+<br><h3 style="text-align: center;">Dealer Management System</h3>
+<?php include('Model/header.php') ?>
+
+<div class="wrapper">
+<p style="text-align: right; font-size: 30px;"><a href="readme">i</a></p>
+<!--ADMIN-->
+<ul>
+  <br><li style="font-size: 25px;"><img src="CSS/admin.png"> Admin</li>
+  <ul>
+    <br><li>Monitor Full Software</li>
+  </ul>
+  <ul>
+    <li>Manage Registration</li>
+  </ul>  
+  <ul>
+    <li>Manage Dealers, Purchase Department Stuffs, Customers</li>
+  </ul>
+  <ul>
+    <li>Add, Update, Remove Company</li><br>
+  </ul>
+</ul>
+
+<!--DEALER-->
+<ul>
+  <li style="font-size: 25px;"><img src="CSS/dealer.png"> Dealer</li>
+  <ul>
+    <br><li>Log In To System</li>
+  </ul>
+  <ul>
+    <li>Add, Update, View Products</li>
+  </ul>
+  <ul>
+    <li>Manage Complains</li>
+  </ul>
+  <ul>
+    <li>Update Profile</li><br>
+  </ul>
+</ul>
+
+<!--PURCHASE DEPARTMENT-->
+<ul>
+  <li style="font-size: 25px;"><img src="CSS/purchase_department.png"> Purchase Department</li>
+  <ul>
+    <br><li>Log In To System</li>
+  </ul> 
+  <ul>
+    <li>Receive Orders</li>
+  </ul>
+  <ul>
+    <li>Check Order Details</li>
+  </ul>
+  <ul>
+    <li>Print Orders By Order Id</li>
+  </ul> 
+  <ul>
+    <li>Update Profile</li><br>
+  </ul>
+</ul>
+
+<!--CUSTOMER-->
+<ul>
+  <li style="font-size: 25px;"><img src="CSS/customer.png"> Customer</li>
+  <ul>
+    <br><li>Register And Login To System</li>
+  </ul> 
+  <ul>
+    <li>Place Items In Cart</li>
+  </ul> 
+  <ul>
+    <li>Confirm Order</li>
+  </ul>
+  <ul>
+    <li>View Previous Orders</li>
+  </ul>
+  <ul>
+    <li>Update Profile</li><br>
+  </ul>
+</ul>
+
+</div>
+
+</body>
+</html>
